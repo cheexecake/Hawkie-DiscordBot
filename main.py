0c8af01a015f8902dc91ef2650a267d1649b91d3
Thando Nyamutumbu <thandon57@gmail.com>
2026-05-17 18:02:19 -0400

    remove .env from tracking

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+import discord
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}!')
+    
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+    print(f'Message from {message.author}: {message.content}')
+
+client.run(token)
