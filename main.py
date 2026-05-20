4909a7bd6d5f030825863128a664b40d5bfaee14
Thando Nyamutumbu <thandon57@gmail.com>
2026-05-19 22:13:01 -0400

    added embed functionality

--- main.py
+++ main.py
@@ -4,22 +4,48 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
 import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ext.commands import Bot
 
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
+intents.message_content = True
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+
+@bot.command
+async def embed(ctx):
+    help = discord.Embed(
+        title="Help",
+        description='Here are a list of commands and what they do:',
+        color=discord.Color.blue()
+    )
+    await ctx.send(embed=help)
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}!')
+    print(f'We have logged in as {bot.user}!')
     
 
-@client.event
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send('Hello! {message.author}')
     print(f'Message from {message.author}: {message.content}')
 
-client.run(token)
+    if message.content.startswith('!help'):
+        help = discord.Embed(
+            title="Help",
+        description='Here are a list of commands and what they do:',
+        color=discord.Color.blue()
+    )
+    await message.channel.send(embed=help)
+    await bot.process_commands(message)
+
+
+bot.run(token)
