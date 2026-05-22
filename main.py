0ef8a5f03c7f3a1601da6add87cfe8f70052857c
Thando Nyamutumbu <thandon57@gmail.com>
2026-05-21 21:30:24 -0400

    Made functional slash commands so users can start the bot with the /cmnd in chat

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from discord.ext.commands import Bot
+from discord import Object
 
 intents = discord.Intents.all()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
-@bot.command
+@bot.event
+async def on_ready():
+    await bot.tree.sync(guild=GUILD_ID)
+    print(f'We have logged in as {bot.user}!')
+
+@bot.command()
 async def embed(ctx):
     help = discord.Embed(
         title="Help",
@@ -22,12 +28,15 @@ async def embed(ctx):
     )
     await ctx.send(embed=help)
 
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}!')
+GUILD_ID = discord.Object(id=1386779222806106193)
     
+@bot.tree.command(name="help", description="Brings out the help prompt and explains all the commands", guild=GUILD_ID)
+async def slashHelp(interaction: discord.Interaction):
+    await interaction.response.send_message("Here are a list of commands and what they do")
 
+@bot.tree.command(name="1v1me", description="Challange another user to a 1v1.", guild=GUILD_ID)
+async def oneVsOne(interaction: discord.Interaction):
+    await interaction.followup(active_matches = {player1 : player2 })
 
 @bot.event
 async def on_message(message):
