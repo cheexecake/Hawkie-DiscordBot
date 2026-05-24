e11688b6b1973936c8513afee7de1143d41e02e8
Thando Nyamutumbu <thando@Thandos-MacBook-Air.local>
2026-05-24 00:08:07 -0400

    Added timeout functionality to the 1v1 bot to prevent players just forgetting and actively waiting

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
+import asyncio
+
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -35,8 +37,16 @@ async def slashHelp(interaction: discord.Interaction):
     await interaction.response.send_message("Here are a list of commands and what they do")
 
 @bot.tree.command(name="1v1me", description="Challange another user to a 1v1.", guild=GUILD_ID)
+#async def ask(interaction):
+ #   await interaction.response.send_message(f"{interaction.user.mention}, please enter a username:")
 async def oneVsOne(interaction: discord.Interaction):
-    await interaction.followup(active_matches = {player1 : player2 })
+    await interaction.response.send_message('Who do you challange?')
+    
+    try:
+        await bot.wait_for('message', timeout=15.0)
+    except asyncio.TimeoutError:
+        await interaction.followup.send('Took too long. Session ended.')
+
 
 @bot.event
 async def on_message(message):
@@ -47,14 +57,6 @@ async def on_message(message):
         await message.channel.send('Hello! {message.author}')
     print(f'Message from {message.author}: {message.content}')
 
-    if message.content.startswith('!help'):
-        help = discord.Embed(
-            title="Help",
-        description='Here are a list of commands and what they do:',
-        color=discord.Color.blue()
-    )
-    await message.channel.send(embed=help)
     await bot.process_commands(message)
-
 
 bot.run(token)
